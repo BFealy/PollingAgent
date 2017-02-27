59b0f7a57d901eecb06540d2731d6ec63931040a
BFealy <bfealy@gmail.com>
2017-02-27 01:07:49 -0500

    Iteration 1
    First iteration, can make the ping to web page and report to the event log, hit a bit of a wall on the timer making it work to poll regularly.

---
+++ WhatsMyIPAgent/ProjectInstaller.vb
@@ -0,0 +1,34 @@
+﻿Imports System.ComponentModel
+Imports System.Configuration.Install
+
+Public Class ProjectInstaller
+
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Component Designer.
+        InitializeComponent()
+
+        'Add initialization code after the call to InitializeComponent
+        Dim sSource As String
+        Dim sLog As String
+        Dim sMachine As String
+
+        sSource = "WhatsMyIPAgent"
+        sLog = "Application"
+        sMachine = "."
+
+        Dim mySourceData As EventSourceCreationData = New EventSourceCreationData("", "")
+        Dim registerSource As Boolean = True
+
+        mySourceData.Source = sSource
+        mySourceData.LogName = sLog
+        mySourceData.MachineName = sMachine
+
+        If Not System.Diagnostics.EventLog.SourceExists(sSource, sMachine) Then
+            Diagnostics.EventLog.CreateEventSource(mySourceData)
+        End If
+
+    End Sub
+
+End Class
